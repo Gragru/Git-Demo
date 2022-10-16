cee859477586065808586b2d25df32185755941d
Mikael Engström <micke@primat.se>
2022-10-16 20:33:25 +0200

    Update Program.cs

--- MinGITDemo/MinGITDemo/Program.cs
+++ MinGITDemo/MinGITDemo/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hej, världen!");
+            Console.WriteLine("Hej, världen, läget!");
         }
     }
 }
